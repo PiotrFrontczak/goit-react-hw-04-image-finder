b17089c0e14bca6c4634f420d752ddfa3e9e6bc1
Piotr Frontczak <piotrf2023@gmail.com>
2024-06-17 00:18:34 +0200

    upd

--- image-search-app/src/App.jsx
+++ image-search-app/src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Component } from 'react';
+import axios from 'axios';
+import Searchbar from './components/Searchbar';
+import ImageGallery from './components/ImageGallery';
+import Loader from './components/Loader';
+import Button from './components/Button';
+import Modal from './components/Modal';
+import './styles.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const API_KEY = '43689937-ac603d3a8790355bd35895aa3';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+class App extends Component {
+  state = {
+    images: [],
+    loading: false,
+    error: null,
+    query: '',
+    page: 1,
+    showModal: false,
+    largeImageURL: '',
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.query !== this.state.query) {
+      this.fetchImages();
+    }
+  }
+
+  fetchImages = () => {
+    const { query, page } = this.state;
+    const url = `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
+
+    this.setState({ loading: true });
+
+    axios
+      .get(url)
+      .then((response) => {
+        this.setState((prevState) => ({
+          images: [...prevState.images, ...response.data.hits],
+          page: prevState.page + 1,
+        }));
+      })
+      .catch((error) => this.setState({ error }))
+      .finally(() => this.setState({ loading: false }));
+  };
+
+  handleSearchSubmit = (query) => {
+    this.setState({ query, images: [], page: 1 });
+  };
+
+  handleImageClick = (largeImageURL) => {
+    this.setState({ largeImageURL, showModal: true });
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+
+  render() {
+    const { images, loading, showModal, largeImageURL } = this.state;
+
+    return (
+      <div className="App">
+        <Searchbar onSubmit={this.handleSearchSubmit} />
+        <ImageGallery images={images} onImageClick={this.handleImageClick} />
+        {loading && <Loader />}
+        {images.length > 0 && !loading && <Button onClick={this.fetchImages} />}
+        {showModal && <Modal largeImageURL={largeImageURL} onClose={this.toggleModal} />}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
